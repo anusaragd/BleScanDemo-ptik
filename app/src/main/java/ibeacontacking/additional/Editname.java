fa46e8ebf175defb7e33d17ee96f27b1eb211389
MASTERS <anusara.orm@gmail.com>
2018-05-21 10:18:25 +0700

    add editname activity

---
+++ app/src/main/java/ibeacontacking/additional/Editname.java
@@ -0,0 +1,15 @@
+package ibeacontacking.additional;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.anusara.ibeacontacking.R;
+
+public class Editname extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editname);
+    }
+}
